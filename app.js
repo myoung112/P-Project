f89e52ef5634fd0e42a2e478157b48b7220fb413
bbasung1 <bbasung1@daum.net>
2023-12-04 16:00:55 +0900

    server_setting

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
+// 
 const express = require('express');
-const path = require('path');
 const app = express();
+const path = require('path');  // path 모듈 사용
 
-// Vue.js 애플리케이션 정적 파일 제공
-app.use(express.static(path.join(__dirname, 'src')));
+app.listen(60020, ()=>{
+    console.log('server on');
+})
 
-// API 엔드포인트
-app.get('/api/data', (req, res) => {
-  // API 로직 구현
-  res.json({ message: 'Hello from backend!' });
-});
-
-// Vue.js 애플리케이션으로 모든 요청을 보냄
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src', 'App.vue'));
-});
-
-const PORT = process.env.PORT || 60020;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.use( '/', express.static( path.join(__dirname, '/dist') ));  
+// 이 부분이 없으면 아래코드에서 index.html을 로드하지 못한다.
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/dist/index.html'));  
+})
+// 기본 경로 '/'을 통해 빌드된 dist/index.html 파일을 로드시킨다
