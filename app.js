408365ee9691c2a665d033820dbba3c7f36e985c
bbasung1 <bbasung1@daum.net>
2023-12-06 13:01:24 +0900

    /api/predict (WIP)

--- app.js
+++ app.js
@@ -12,4 +12,13 @@ app.use( '/', express.static( path.join(__dirname, '/dist') ));
 app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, '/dist/index.html'));  
 })
+app.get('/api/predict',(req,res)=>{
+    res.json({
+        "음식명": "(검은)콩조림",
+        "지방(g)": 2.09,
+        "탄수화물(g)": 6.98,
+        "단백질(g)": 3.85,
+        "에너지(kcal)": 56.83
+    })
+})
 // 기본 경로 '/'을 통해 빌드된 dist/index.html 파일을 로드시킨다
